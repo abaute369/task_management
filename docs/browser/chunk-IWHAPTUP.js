324754c0fdc9257de7f2a044dca4406202f9ef97
abaute202@gmail.com <abaute202@gmail.com>
2024-07-07 13:06:54 +0530

    newly add build

---
+++ docs/browser/chunk-IWHAPTUP.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-KAN3FHVZ.js";import"./chunk-CH5TLQ2K.js";import"./chunk-K6TD2CPM.js";import"./chunk-SN6OR5EP.js";import"./chunk-CWTPBX7D.js";export{a as LoginModule};
---
+++ docs/browser/chunk-LLB2YYZQ.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-CH5TLQ2K.js";import"./chunk-CWTPBX7D.js";export default a();
