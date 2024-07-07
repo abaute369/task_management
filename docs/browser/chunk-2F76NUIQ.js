6457f403ee2daf9388a1bad470e5e5c10d10ec80
abaute202@gmail.com <abaute202@gmail.com>
2024-07-07 13:29:20 +0530

    deleted docs

--- docs/browser/chunk-2F76NUIQ.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-IFYVNZWW.js";import"./chunk-K6TD2CPM.js";import"./chunk-SN6OR5EP.js";import"./chunk-CWTPBX7D.js";export{a as RegistrationModule};
--- docs/browser/chunk-LLB2YYZQ.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-CH5TLQ2K.js";import"./chunk-CWTPBX7D.js";export default a();
