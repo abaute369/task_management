de3af1cfb696190bf2eb4c5f921e79f61e1c98dc
abaute202@gmail.com <abaute202@gmail.com>
2024-07-07 10:10:16 +0530

    removing sweetalert

---
+++ docs/browser/chunk-4ZC57HDF.js
@@ -0,0 +1,1 @@
+import{w as a}from"./chunk-ZVW2NKDF.js";import"./chunk-CH5TLQ2K.js";import"./chunk-K6TD2CPM.js";import"./chunk-AESYL7IM.js";import"./chunk-SN6OR5EP.js";import"./chunk-CWTPBX7D.js";export{a as DashboardModule};
